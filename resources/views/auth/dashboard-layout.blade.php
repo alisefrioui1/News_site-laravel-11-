d1356a22599affc13e651d02f8cb231ad522a80f
alisefrioui1 <alielsefriwi@gmail.com>
2024-07-29 10:42:27 +0100

    errors msg validation

--- resources/views/auth/dashboard-layout.blade.php
+++ resources/views/auth/dashboard-layout.blade.php
@@ -7,34 +7,33 @@
       content="width=device-width, initial-scale=1.0, user-scalable=0"
     />
     <title>Admin Dashboard</title>
-    {# Dashboard - Links #}
     <link
       rel="shortcut icon"
       type="image/x-icon"
-      href="assets/img/favicon.png"
+      href=" {{ asset('Back_auth/assets/img/favicon.png')}}"
     />
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="{{ asset('Back_auth/assets/css/bootstrap.min.css')}}" />
     <link
       rel="stylesheet"
-      href="assets/plugins/fontawesome/css/fontawesome.min.css"
+      href="{{ asset('Back_auth/assets/plugins/fontawesome/css/fontawesome.min.css')}}"
     />
-    <link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css" />
-    <link rel="stylesheet" href="assets/css/feathericon.min.css" />
-    <link rel="stylehseet" href="https://cdn.oesmith.co.uk/morris-0.5.1.css" />
-    <link rel="stylesheet" href="assets/plugins/morris/morris.css" />
-    <link rel="stylesheet" href="assets/css/style.css" />
-    {# Fin Dashbord Link #}
+    <link rel="stylesheet" href="{{ asset('Back_auth/assets/plugins/fontawesome/css/all.min.css')}}" />
+    <link rel="stylesheet" href="{{ asset('Back_auth/assets/css/feathericon.min.css')}}" />
+    <link rel="stylehseet" href="{{ asset('Back_auth/https://cdn.oesmith.co.uk/morris-0.5.1.css')}}" />
+    <link rel="stylesheet" href="{{ asset('Back_auth/assets/plugins/morris/morris.css')}}" />
+    <link rel="stylesheet" href="{{ asset('Back_auth/assets/css/style.css')}}" />
+   // Fin Dashbord Link 
   </head>
 
   <body>
-    {# Main wrapper #}
+   // Main wrapper 
     <div class="main-wrapper">
-      {# Debut Header #}
+     // Debut Header #
       <div class="header">
         <div class="header-left">
           <a href="index.html" class="logo">
             <img
-              src="assets/img/logo.png"
+              src="{{ asset('Back_auth/assets/img/logo.png')}}"
               width="50"
               height="70"
               alt="logo"
@@ -43,7 +42,7 @@
           </a>
           <a href="index.html" class="logo logo-small">
             <img
-              src="assets/img/logo.png"
+              src="{{ asset('Back_auth/assets/img/logo.png')}}"
               alt="Logo"
               width="30"
               height="30"
@@ -61,7 +60,7 @@
               <span class="user-img"
                 ><img
                   class="rounded-circle"
-                  src="assets/img/profiles/avatar-01.png"
+                  src="{{ asset('Back_auth/assets/img/profiles/avatar-01.png')}} "
                   width="31"
                   alt="John Doe"
               /></span>
@@ -70,7 +69,7 @@
               <div class="user-header">
                 <div class="avatar avatar-sm">
                   <img
-                    src="assets/img/profiles/avatar-01.png"
+                    src="{{ asset('Back_auth/assets/img/profiles/avatar-01.png')}} "
                     alt="User Image"
                     class="avatar-img rounded-circle"
                   />
@@ -82,7 +81,12 @@
               </div>
               <a class="dropdown-item" href="profile.html">Profile</a>
               <a class="dropdown-item" href="settings.html">Paramettre</a>
-              <a class="dropdown-item" href="login.html">Deconnexion</a>
+
+              <!-- Add a form for logout -->
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+            <a class="dropdown-item" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Deconnexion</a>
             </div>
           </li>
         </ul>
@@ -95,9 +99,9 @@
           </form>
         </div>
       </div>
-      {# Fin Header #} 
-      {# ------------------ #}
-      {# Debut Sidebar #}
+     // Fin Header  
+     // ------------------ 
+     // Debut Sidebar 
       <div class="sidebar" id="sidebar">
         <div class="sidebar-inner slimscroll">
           <div id="sidebar-menu" class="sidebar-menu">
@@ -198,219 +202,40 @@
           </div>
         </div>
       </div>
-      {# Fin Sidebar #} {# --------------------- #} {# Contenu de la page #}
+     // Fin Sidebar // --------------------- // Contenu de la page 
       <div class="page-wrapper">
         <div class="content container-fluid">
           <div class="page-header">
             <div class="row">
               <div class="col-sm-12 mt-5">
-                <h3 class="page-title mt-3">Hello, Bonjour John Doe!</h3>
+                <h3 class="page-title mt-3">Hello, Bonjour USER</h3>
                 <ul class="breadcrumb">
                   <li class="breadcrumb-item active">Dashboard</li>
                 </ul>
               </div>
             </div>
           </div>
-          <div class="row">
-            <div class="col-xl-3 col-sm-6 col-12">
-              <div class="card board1 fill">
-                <div class="card-body">
-                  <div class="dash-widget-header">
-                    <div>
-                      <h3 class="card_widget_header">236</h3>
-                      <h6 class="text-muted">Total Articles</h6>
-                    </div>
-                    <div class="ml-auto mt-md-3 mt-lg-0">
-                      <span class="opacity-7 text-muted"
-                        ><svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="24"
-                          height="24"
-                          viewbox="0 0 24 24"
-                          fill="none"
-                          stroke="#009688"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          class="feather feather-user-plus"
-                        >
-                          <path
-                            d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"
-                          ></path>
-                          <circle cx="8.5" cy="7" r="4"></circle>
-                          <line x1="20" y1="8" x2="20" y2="14"></line>
-                          <line x1="23" y1="11" x2="17" y2="11"></line></svg
-                      ></span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-xl-3 col-sm-6 col-12">
-              <div class="card board1 fill">
-                <div class="card-body">
-                  <div class="dash-widget-header">
-                    <div>
-                      <h3 class="card_widget_header">10</h3>
-                      <h6 class="text-muted">Total Catégories</h6>
-                    </div>
-                    <div class="ml-auto mt-md-3 mt-lg-0">
-                      <span class="opacity-7 text-muted"
-                        ><svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="24"
-                          height="24"
-                          viewbox="0 0 24 24"
-                          fill="none"
-                          stroke="#009688"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          class="feather feather-dollar-sign"
-                        >
-                          <line x1="12" y1="1" x2="12" y2="23"></line>
-                          <path
-                            d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"
-                          ></path></svg
-                      ></span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-xl-3 col-sm-6 col-12">
-              <div class="card board1 fill">
-                <div class="card-body">
-                  <div class="dash-widget-header">
-                    <div>
-                      <h3 class="card_widget_header">1538</h3>
-                      <h6 class="text-muted">Total Commentaires</h6>
-                    </div>
-                    <div class="ml-auto mt-md-3 mt-lg-0">
-                      <span class="opacity-7 text-muted"
-                        ><svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="24"
-                          height="24"
-                          viewbox="0 0 24 24"
-                          fill="none"
-                          stroke="#009688"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          class="feather feather-file-plus"
-                        >
-                          <path
-                            d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"
-                          ></path>
-                          <polyline points="14 2 14 8 20 8"></polyline>
-                          <line x1="12" y1="18" x2="12" y2="12"></line>
-                          <line x1="9" y1="15" x2="15" y2="15"></line></svg
-                      ></span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-xl-3 col-sm-6 col-12">
-              <div class="card board1 fill">
-                <div class="card-body">
-                  <div class="dash-widget-header">
-                    <div>
-                      <h3 class="card_widget_header">364</h3>
-                      <h6 class="text-muted">Abonnements</h6>
-                    </div>
-                    <div class="ml-auto mt-md-3 mt-lg-0">
-                      <span class="opacity-7 text-muted"
-                        ><svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="24"
-                          height="24"
-                          viewbox="0 0 24 24"
-                          fill="none"
-                          stroke="#009688"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          class="feather feather-globe"
-                        >
-                          <circle cx="12" cy="12" r="10"></circle>
-                          <line x1="2" y1="12" x2="22" y2="12"></line>
-                          <path
-                            d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"
-                          ></path></svg
-                      ></span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+         
+         //here the content       
+         @yield('content')
 
-          <div class="row">
-            <div class="col-md-12 d-flex">
-              <div class="card card-table flex-fill">
-                <div class="card-header">
-                  <h4 class="card-title float-left mt-2">Articles recents</h4>
-                  <button
-                    type="button"
-                    class="btn btn-primary float-right veiwbutton"
-                  >
-                    Voir tous
-                  </button>
-                </div>
-                <div class="card-body">
-                  <div class="table-responsive">
-                    <table class="table table-hover table-center">
-                      <thead>
-                        <tr>
-                          <th>ID Articles</th>
-                          <th>Image</th>
-                          <th>Titre</th>
-                          <th>Catégories</th>
-                          <th class="text-right">Auteur</th>
-                          <th class="text-center">Publication</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <td class="text-nowrap">
-                            <div>ART-0001</div>
-                          </td>
-                          <td class="text-nowrap"></td>
-                          <td>Intelligence artificielle</td>
-                          <td>Tech</td>
-                          <td class="text-center">John Doe</td>
-                          <td class="text-center">
-                            <span class="badge badge-pill bg-success inv-badge"
-                              >PUBLIE</span
-                            >
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
-      {# Fin Contenu de la page #}
+     // Fin Contenu de la page 
     </div>
-    {# Scripts dashboard #}
+   // Scripts dashboard 
     <script
       data-cfasync="false"
-      src="../../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"
+      src="{{ asset('Back_auth/../../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js')}}"
     ></script>
-    <script src="assets/js/jquery-3.5.1.min.js"></script>
-    <script src="assets/js/popper.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
-    <script src="assets/plugins/raphael/raphael.min.js"></script>
-    <script src="assets/plugins/morris/morris.min.js"></script>
-    <script src="assets/js/chart.morris.js"></script>
-    <script src="assets/js/script.js"></script>
-    {# Fin Script Dashboard #}
+    <script src="{{ asset('Back_auth/assets/js/jquery-3.5.1.min.js')}}"></script>
+    <script src="{{ asset('Back_auth/assets/js/popper.min.js')}}"></script>
+    <script src="{{ asset('Back_auth/assets/js/bootstrap.min.js')}}"></script>
+    <script src="{{ asset('Back_auth/assets/plugins/slimscroll/jquery.slimscroll.min.js')}}"></script>
+    <script src="{{ asset('Back_auth/assets/plugins/raphael/raphael.min.js')}}"></script>
+    <script src="{{ asset('Back_auth/assets/plugins/morris/morris.min.js')}}"></script>
+    <script src="{{ asset('Back_auth/assets/js/chart.morris.js')}}"></script>
+    <script src="{{ asset('Back_auth/assets/js/script.js')}}"></script>
+   // Fin Script Dashboard 
   </body>
 </html>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,16 +7,31 @@
 <h1>LOGIN</h1>
 							<p class="account-subtitle">login to dashboard</p>
 							<form method="POST" action="{{ route('login') }}">
-                                @csrf
-
+								@csrf
+							
 								<div class="form-group">
-									<input class="form-control" type="text" name="email" placeholder="Email"> </div>
+									<input class="form-control @error('email') is-invalid @enderror" type="text" name="email" value="{{ old('email') }}" placeholder="Email">
+									@error('email')
+										<div class="invalid-feedback">
+											{{ $message }}
+										</div>
+									@enderror
+								</div>
+							
 								<div class="form-group">
-									<input class="form-control" type="password" name="password" placeholder="Mot de passe"> </div>
+									<input class="form-control @error('password') is-invalid @enderror" type="password" name="password" placeholder="Mot de passe">
+									@error('password')
+										<div class="invalid-feedback">
+											{{ $message }}
+										</div>
+									@enderror
+								</div>
+							
 								<div class="form-group">
 									<button class="btn btn-primary btn-block" type="submit">Login</button>
 								</div>
 							</form>
+							
 							<div class="text-center forgotpass"><a href="{{route("password.request")}}">Forget password?</a> </div>
 							
 							<div class="text-center dont-have">if you don't have an account  <a href="{{ route('register') }}">regester</a></div>
